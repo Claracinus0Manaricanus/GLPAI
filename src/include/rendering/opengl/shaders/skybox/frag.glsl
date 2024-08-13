77a61e2eb418a193850f6b51d68566ff99fa7fa2
Claracinus0Manaricanus <akkayasarp4@gmail.com>
2024-08-13 09:19:40 +0300

    quick glTexParamteri fix for cubemaps

--- src/include/rendering/opengl/shaders/skybox/frag.glsl
+++ src/include/rendering/opengl/shaders/skybox/frag.glsl
@@ -7,6 +7,5 @@ out vec4 col;
 uniform samplerCube cubemap;
 
 void main() {
-    col = vec4((length(vec3(1, 1, 1)) - length(fragPos)).xxx, 1);
-    col = vec4(texture(cubemap, fragPos).xyz, 1);
+    col = vec4(texture(cubemap, vec3(-fragPos.x, fragPos.y, fragPos.z)).xyz, 1);
 }
