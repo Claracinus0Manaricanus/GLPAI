beb03c7739b723a55d39239caab6ffdede795ab3
Claracinus0Manaricanus <akkayasarp4@gmail.com>
2024-09-29 19:08:21 +0300

    started working on a tree data structure

---
+++ src/include/types/data_structures/tree.cpp
@@ -0,0 +1,35 @@
+#include "tree.hpp"
+
+// constructors
+template <class T> cm_Tree<T>::cm_Tree() {
+  root.parent = nullptr;
+  root.value = nullptr;
+  root.childCount = 0;
+  root.childs = nullptr;
+}
+template <class T> cm_Tree<T>::cm_Tree(cm_TreeData data) {
+  root.parent = nullptr;
+  root.value = nullptr;
+  root.childCount = 0;
+  root.childs = nullptr;
+}
+
+// destructors
+
+template <class T> void helperFreeFunc(TreeNode<T> node) {
+  for (int i = 0; i < node.childCount; i++) {
+    helperFreeFunc<T>(node.childs[i]);
+    free(node.childs[i]);
+  }
+
+  free(node.childs);
+}
+
+template <class T> cm_Tree<T>::~cm_Tree() {
+  //
+  helperFreeFunc<T>(root);
+}
+
+// setters
+
+// getters
---
+++ src/include/types/data_structures/tree.hpp
@@ -0,0 +1,45 @@
+#ifndef CM_TREE
+#define CM_TREE
+
+struct cm_TreeData {
+  //
+};
+
+template <class T> struct TreeNode {
+  TreeNode* parent;
+
+  T* value;
+
+  int childCount;
+  TreeNode** childs;
+};
+
+template <class T> class cm_Tree {
+protected:
+  TreeNode<T> root;
+
+public:
+  // constructors
+  cm_Tree();
+  cm_Tree(cm_TreeData data);
+
+  // destructors
+  ~cm_Tree();
+
+  // setters, for node variable if nullptr is given it means root node
+  TreeNode<T>* addChildTo(
+      TreeNode<T>* node,
+      T* toAddAsChild); // returns a pointer to the newly created node
+
+  // getters, for node variable if nullptr is given it means root node
+  TreeNode<T>& getRootNode(); // returns root node
+
+  TreeNode<T>* getChilds(TreeNode<T>* node,
+                         int& childCount); // returns all childs of a node and
+                                           // puts the count into childCount
+
+  TreeNode<T>* getChild(TreeNode<T>* node,
+                        int n); // returns the n'th child of a node
+};
+
+#endif
